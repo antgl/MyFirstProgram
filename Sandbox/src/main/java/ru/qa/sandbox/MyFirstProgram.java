0871c91d5b9c2ddf53eeab79ac2d2519ecd88d70
antgl <antonglazdovsky@gmail.com>
2017-10-25 15:46:02 +0300

    1

--- Sandbox/src/main/java/ru/qa/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/qa/sandbox/MyFirstProgram.java
@@ -4,6 +4,19 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+
+        Square s = new Square(5);
+        System.out.println("Square area is " + s.area());
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Rectangle area is " + r.area());
     }
 
-} 
+
+
+
+
+
+}
+
