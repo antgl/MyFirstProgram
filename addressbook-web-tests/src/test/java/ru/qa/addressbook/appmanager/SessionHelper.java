ef1121c40e17612280d3b66a38ccfa4a36097361
antgl <antonglazdovsky@gmail.com>
2017-10-29 22:13:36 +0300

    test

---
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,26 @@
+package ru.qa.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SessionHelper extends HelperBase {
+
+
+    public SessionHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+
+
+    public void login(String username, String password) {
+        type(By.name("user"), username);
+        type(By.name("pass"), password);
+        click(By.xpath("//form[@id='LoginForm']/input[3]"));
+    }
+
+    public void logout() {
+        click(By.linkText("Выйти"));
+
+    }
+
+
+}
